522700b6efbcddd98547c1105fb12f402f23a837
austin imbastari <austin imbastari>
2017-11-22 05:29:03 -0800

    initial commit

---
+++ src/PipelineContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class PipelineContainer extends Component{
+
+    render(){
+        return(
+           <div>
+                <div>
+                    icons
+                </div>
+                <div>
+                    data canvas
+                </div>  
+
+           </div>
+
+
+        );
+    }
+
+
+} 
+
+export default PipelineContainer;
